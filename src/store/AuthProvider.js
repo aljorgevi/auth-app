bea0636fbc7c2330f605bd70df0f9a22fd448da0
aljorgevi <jormencar@yahoo.com>
2022-05-02 17:14:22 +0100

    code done

--- src/store/AuthProvider.js
+++ src/store/AuthProvider.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import AuthContext from './auth_context'
 
 // this could be in a service
@@ -11,26 +11,60 @@ const calculateRemainingTime = expirationTime => {
   return remainingTime
 }
 
+let logoutTimer
+
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem('token')
+  const storedExpirationDate = localStorage.getItem('expirationTime')
+
+  const remainingTime = calculateRemainingTime(storedExpirationDate)
+
+  if (remainingTime <= 60000) {
+    localStorage.removeItem('token')
+    localStorage.removeItem('expirationTime')
+    return null
+  }
+
+  return { token: storedToken, duration: remainingTime }
+}
+
 export const AuthProvider = ({ children }) => {
-  const initialToken = localStorage.getItem('token')
+  const tokenData = retrieveStoredToken()
+  const initialToken = tokenData ? tokenData.token : null
   const [token, setToken] = useState(initialToken)
 
   // !!token is a boolean that is true if token is a string is not empty, and false if is a empty string.
   const userIsLoggenIn = !!token
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null)
     localStorage.removeItem('token')
-  }
+    localStorage.removeItem('expirationTime')
+
+    // clear the logout timer if user want to logout manually
+    if (logoutTimer) {
+      clearTimeout(logoutTimer)
+    }
+  }, [])
 
   const loginHandler = (token, expirationTime) => {
     setToken(token)
     localStorage.setItem('token', token)
+    // expirationTime has to be String and it is, in fact, a string(comes from where is passed it from)
+    localStorage.setItem('expirationTime', expirationTime)
 
     const remainingTime = calculateRemainingTime(expirationTime)
 
-    setTimeout(logoutHandler, remainingTime)
+    // setTimeout return a reference we save it in logoutTimer
+    logoutTimer = setTimeout(logoutHandler, remainingTime)
   }
+
+  useEffect(() => {
+    if (tokenData) {
+      console.log(tokenData.duration)
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration)
+    }
+  }, [tokenData, logoutHandler])
 
   return (
     <AuthContext.Provider
