069396232820d1d4c1aa3e1f5f6c4121265c6063
aljorgevi <jormencar@yahoo.com>
2022-05-02 16:42:24 +0100

    adding localStorage

--- src/store/AuthProvider.js
+++ src/store/AuthProvider.js
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import AuthContext from './auth_context'
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null)
+  const initialToken = localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken)
 
   // !!token is a boolean that is true if token is a string is not empty, and false if is a empty string.
   const userIsLoggenIn = !!token
 
   const loginHandler = token => {
     setToken(token)
+    localStorage.setItem('token', token)
   }
 
   const logoutHandler = () => {
     setToken(null)
+    localStorage.removeItem('token')
   }
 
   return (
